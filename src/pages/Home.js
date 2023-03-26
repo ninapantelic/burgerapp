01c4310631a40753eea5811d8c542cdd24272b25
Nina Pantelic <nina.pantelic20@gmail.com>
2023-03-27 00:08:21 +0200

    Stranica Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import Banner from "../assets/burger.jpg";
+import "../styles/Home.css";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home" style={{ backgroundImage: `url(${Banner})` }}>
+      <div className="headerContainer">
+        <h1>React Burger</h1>
+        <Link to="/menu">
+          <button>ORDER NOW</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
