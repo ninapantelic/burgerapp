e297a61101ea941387902188a02c34817c561873
Nina Pantelic <nina.pantelic20@gmail.com>
2023-03-27 20:10:35 +0200

    Dodat opis

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AboutUs from "../assets/about.jpg";
+import "../styles/About.css";
 
 function About() {
   return (
@@ -17,7 +18,17 @@ function About() {
           1954, Burger React is the second largest fast food hamburger chain in
           the world. The original Home of the Whopper, our commitment to premium
           ingredients, signature recipes, and family-friendly dining experiences
-          is what has defined our brand for more than 50 successful years.
+          is what has defined our brand for more than 50 successful years. A
+          hamburger, or simply burger, is a sandwich consisting of
+          fillings—usually a patty of ground meat, typically beef—placed inside
+          a sliced bun or bread roll. Hamburgers are often served with cheese,
+          lettuce, tomato, onion, pickles, bacon, or chilis; condiments such as
+          ketchup, mustard, mayonnaise, relish, or a "special sauce," often a
+          variation of Thousand Island dressing; and are frequently placed on
+          sesame seed buns. A hamburger patty topped with cheese is called a
+          cheeseburger. The term burger can also be applied to the meat patty on
+          its own, especially in the United Kingdom, where the term patty is
+          rarely used or can even refer to ground beef.
         </p>
       </div>
     </div>
