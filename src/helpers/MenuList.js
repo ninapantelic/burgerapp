56fe97ef44095137ab49f43d4df5d270de8db4ae
Nina Pantelic <nina.pantelic20@gmail.com>
2023-03-27 22:18:36 +0200

    Kreirana lista burgera iz menija

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Whopper from "../assets/whopper.png";
+import Hamburger from "../assets/hamburger.png";
+import Cheeseburger from "../assets/cheeseburger.png";
+import King from "../assets/king.png";
+import BigTasty from "../assets/big.png";
+import DoubleWhopper from "../assets/double.png";
+
+export const MenuList = [
+  {
+    name: "Whopper",
+    image: Whopper,
+    price: 9.99,
+  },
+  {
+    name: "Hamburger",
+    image: Hamburger,
+    price: 4.99,
+  },
+  {
+    name: "Cheeseburger",
+    image: Cheeseburger,
+    price: 5.53,
+  },
+  {
+    name: "King JR",
+    image: King,
+    price: 11.99,
+  },
+  {
+    name: "Big Tasty",
+    image: BigTasty,
+    price: 11.99,
+  },
+  {
+    name: "Double Whopper",
+    image: DoubleWhopper,
+    price: 10.99,
+  },
+];
