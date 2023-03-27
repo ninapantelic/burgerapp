bfa4eeffe6b5fe20ef58cbde80498cffa221f404
Nina Pantelic <nina.pantelic20@gmail.com>
2023-03-27 22:19:00 +0200

    Stranica meni

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,7 +1,26 @@
 import React from "react";
+import MenuItem from "../components/MenuItem";
+import "../styles/Menu.css";
+import { MenuList } from "../helpers/MenuList";
 
 function Menu() {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      <h1 className="menuTitle">Our Menu</h1>
+      <div className="menuList">
+        {MenuList.map((menuItem, index) => {
+          return (
+            <MenuItem
+              key={index}
+              image={menuItem.image}
+              name={menuItem.name}
+              price={menuItem.price}
+            ></MenuItem>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
