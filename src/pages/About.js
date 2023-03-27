0457e24d595050d8f6a7b945675ee8cefc7a9d11
Nina Pantelic <nina.pantelic20@gmail.com>
2023-03-27 20:07:16 +0200

    Napravljena stranica About us

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,27 @@
 import React from "react";
+import AboutUs from "../assets/about.jpg";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${AboutUs})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1> ABOUT US</h1>
+        <p>
+          Every day, more than 11 million guests visit Burger React restaurants
+          around the world. And they do so because our restaurants are known for
+          serving high-quality, great-tasting, and affordable food. Founded in
+          1954, Burger React is the second largest fast food hamburger chain in
+          the world. The original Home of the Whopper, our commitment to premium
+          ingredients, signature recipes, and family-friendly dining experiences
+          is what has defined our brand for more than 50 successful years.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
